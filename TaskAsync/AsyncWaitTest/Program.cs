12c37561c237cb9e64260e4a0476228a59a044bb
sandeep nellutla <sandeepjhankar@gmail.com>
2017-12-25 22:16:35 -0600

    s

---
+++ TaskAsync/AsyncWaitTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncWaitTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RunLoopAsync();
+            Console.WriteLine("Back to main...");
+            Console.ReadKey();
+        }
+
+        private async static Task RunLoopAsync()
+        {
+            int taskid = -1;
+            for (int i = 0; i < 10; i++)
+            {
+                var result =
+                       await Task.Run(() =>
+                        {
+                            Random r = new Random();
+                            var sleepCntr = r.Next(1, 1000);                            
+                            Thread.Sleep(sleepCntr);
+                            taskid = Task.CurrentId??0;
+                            Console.WriteLine($"...{sleepCntr}..."); 
+                            return i;
+                        });
+
+                Console.WriteLine($"--{result}--{taskid}");
+            }
+
+            Console.WriteLine("Completed Loop...");
+        }
+    }
+}
---
+++ TaskAsync/TaskAsync/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool cont = false;
+            Console.WriteLine("Started....");
+
+            var task =
+                    Task.Run(() =>
+                    {
+                        for (int i = 0; i < 10; i++)
+                        {
+                            Console.WriteLine($"....{i}.... ");
+                            Thread.Sleep(2000);
+                        }
+                    });
+
+            task.ContinueWith((t) =>
+            {
+                try
+                {
+                    Console.WriteLine("Async With Task...");
+                    Console.WriteLine("End");
+                    cont = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error:{ex.ToString()}");
+                }
+            });
+
+
+            var task2 =
+                Task.Run(() =>
+                {
+                    while (!cont)
+                    {
+                        Thread.Sleep(4000);
+                        Console.WriteLine($"--{task.AsyncState} --{cont}---{task.IsCompleted}--");
+                    }
+                });
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    while (!task2.IsCompleted)
+                    {
+                        Thread.Sleep(1000);
+                        Console.WriteLine("Task 2 continues.."+ Task.CurrentId);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error Task 2 " + ex.ToString());
+                }
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
