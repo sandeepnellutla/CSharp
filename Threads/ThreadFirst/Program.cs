a3e5c04ce9641b25403a4693a3736641cad06061
sandeep nellutla <sandeepjhankar@gmail.com>
2017-12-29 10:20:34 -0600

    threads

---
+++ Threads/ThreadFirst/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+
+            t.Start();
+
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Main Thread: Do Work...");
+                Thread.Sleep(0);
+            }
+
+            t.Join();
+
+            Console.ReadKey();
+        }
+
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"ThreadProc:{i}");
+                Thread.Sleep(0);
+            }
+        }
+    }
+}
